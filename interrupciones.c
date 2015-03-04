2f876eb21fa67d74f34d0dc6f1543621441049f8
Amadeo de Gracia <amadeodeg@gmail.com>
2015-03-04 17:12:33 +0100

    Creacion fichero interrupciones.c

---
+++ interrupciones.c
@@ -0,0 +1,19 @@
+#include "m5272.h"
+#include "calculos.h" //Funciones llamadas en atencion a la interrupcion
+//TODO include
+
+#ifndef DATOS
+#define V_BASE 0x40   //Direccion inicio (base) de vector interrupciones
+#define DIR_VTMR0 4*(V_BASE+5)	//Direccion del vector de timer0
+#define FREC_INT 4000	//Frecuencia de interrupcion periodica
+#define PRESCALADO 2
+#define CNT_INIT1 MCF_CLK/(FREC_INT*PRESCALADO*16) //TRR, valor de carga en el temporizador
+
+#if CNT_INIT1>0xFFFF
+#error PRESCALADO demasiado pequeño para la frecuencia pedida(CNT_INIT1>0xFFFF)
+#endif
+
+#define BORRA_REF 0x0002	//TODO que es esto?
+
+
+#endif
