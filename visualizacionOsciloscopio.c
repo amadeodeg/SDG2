8b6d2d0f82c18485f1e3639fd8027daee2b012c7
Amadeo de Gracia <amadeodeg@gmail.com>
2015-03-06 19:24:30 +0100

    Funciones Visualizacion

---
+++ visualizacionOsciloscopio.c
@@ -0,0 +1,9 @@
+
+
+void visualizacion_ociloscopio(int[] *puntero){
+	int i;
+	for (i = 0; i < NUM_FREC_MUESTREADAS; i++){
+		DAC_dato(*(puntero+i));
+		sleep(T_REFRESCO/NUM_FREC_MUESTREADAS);
+	}
+}
