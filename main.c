1a6fea39c77f6df5cdb1b3ad75e8185b81848750
Amadeo de Gracia <amadeodeg@gmail.com>
2015-03-12 19:20:43 +0100

    Algoritmo decision salida DAC

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
+void visualizacion_ociloscopio(void){
+	while(1){
+		int i;
+		for (i = 0; i < NUM_FREC_MUESTREADAS; i++){
+			DAC_dato(mod2esc(modFrecTot));
+			sleep(T_PANTALLA/NUM_FREC_MUESTREADAS)
+		}
+	}
+}
 
 int main(int argc, char const *argv[])
 {
-	visualizacion_ociloscopio(&modFrecTot);
+	visualizacion_ociloscopio();
 	return 0;
 }
